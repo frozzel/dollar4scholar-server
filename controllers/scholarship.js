00310b6a81f7a40ad6c0ced703de594a79131c27
Dennis Hickox <frozzel@me.com>
2023-10-19 14:54:06 -0400

    update scholarship to let students enter to win

--- controllers/scholarship.js
+++ controllers/scholarship.js
@@ -25,44 +25,81 @@ exports.addDonorContribution = async (req, res) => {
         if (!user) return sendError(res, 'User not found!', 404)
         if (!scholarship) return sendError(res, 'Scholarship not found!', 404)
 
-        if (req.body.amount > user.wallet) return sendError(res, 'Insufficient funds!', 400)
+        if (req.body.wallet > user.wallet) return sendError(res, 'Insufficient funds!', 400)
 
         const contributions = new Contribution({
             userId: userId,
             scholarshipId: scholarship._id,
-            amount: req.body.amount,
+            amount: req.body.wallet,
             date: new Date()
         });
         
         await contributions.save();
         scholarship.donorContributions.push(contributions._id);
-        scholarship.pot += req.body.amount; // add the amount to the scholarship pot
+        scholarship.pot += req.body.wallet; // add the amount to the scholarship pot
         await scholarship.save();
 
         // Deduct the contribution amount from the user's wallet
 
-        user.wallet -= req.body.amount;
+        user.wallet -= req.body.wallet;
 
         // add the contribution id to the user object
         user.contribution.push(contributions._id);
         await user.save();
 
-        res.status(200).json({ message: 'Donor contribution added successfully', scholarship });
+        const userWallet = user.wallet;
 
+        res.status(200).json({ message: 'Donor contribution added successfully', wallet: userWallet, contributions });
+
+};
+
+// Get current scholarship pot
+exports.getScholarshipPot = async (req, res) => {
+    try {
+        const scholarship = await Scholarship.findOne().sort({ created_at: -1 });
+        if (!scholarship) return sendError(res, 'Scholarship not found!', 404);
+        res.status(200).json({ message: 'Scholarship pot retrieved successfully', scholarship });
+    } catch (error) {
+        res.status(400).json({ message: 'Error retrieving scholarship pot', error });
+    }
+};
+
+// Add money to the pot of a scholarship
+exports.addMoneyToPot = async (req, res) => {
+    const { userId } = req.params;
+    const scholarship = await Scholarship.findOne().sort({ created_at: -1 });
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) return sendError(res, 'User not found!', 404);
+    if (!scholarship) return sendError(res, 'Scholarship not found!', 404);
+
+    if (req.body.wallet > user.wallet) return sendError(res, 'Insufficient funds!', 400);
+
+    const numberOfEntries = Math.floor(req.body.wallet); // Assuming wallet contains a whole number
+
+    for (let i = 0; i < numberOfEntries; i++) {
+        scholarship.studentsEntered.push(user._id);
+    }
+    scholarship.pot += req.body.wallet;
+
+    await Scholarship.findByIdAndUpdate(scholarship._id, scholarship);
+
+     // Deduct the contribution amount from the user's wallet
+
+     user.wallet -= req.body.wallet;
+
+     // add the contribution id to the user object
+     
+     await user.save();
+
+     const userWallet = user.wallet;
+
+    res.status(200).json({ message: 'User added to scholarship successfully', scholarship: scholarship, wallet: userWallet });
 };
 
 
 // Add money to the pot of a scholarship
-exports.addMoneyToPot = async (req, res) => {
-    try {
-        const scholarship = await Scholarship.findById(req.params.id);
-        scholarship.pot += req.body.amount;
-        await scholarship.save();
-        res.status(200).json({ message: 'Money added to pot successfully', scholarship });
-    } catch (error) {
-        res.status(400).json({ message: 'Error adding money to pot', error });
-    }
-};
+
 
 // Set a scholarship as active or not
 exports.setActiveStatus = async (req, res) => {
--- routes/scholarship.js
+++ routes/scholarship.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createScholarship, addDonorContribution} = require('../controllers/scholarship');
+const {createScholarship, addDonorContribution, getScholarshipPot, addMoneyToPot} = require('../controllers/scholarship');
 const {isAuth} = require('../utils/auth');
 
 // Route to create a new scholarship
@@ -8,6 +8,8 @@ router.post('/create', createScholarship);
 
 // Route to add a donation to a scholarship
 router.post('/donate/:userId',isAuth, addDonorContribution);
+router.get('/pot', getScholarshipPot); // Route to get the current scholarship pot
+router.post('/enter/:userId', addMoneyToPot); // Route to add money to the scholarship pot and enter to win the scholarship
 
 
 
