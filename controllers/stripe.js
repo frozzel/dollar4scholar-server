2090b32996b27e59a5c5131a51be3f8b941a9944
Dennis Hickox <frozzel@me.com>
2024-05-31 13:02:33 -0400

    subscription frame working now data needs to sync with database and invoice

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -69,19 +69,74 @@ exports.getSessionStatus = async (req, res) => {
 };
 
 //create subscription
+// exports.createSubscription = async (req, res) => {
+//     // priceId = "price_1PLUlvHiFNaBiQsH7Y22so1X";
+//     console.log(req.body);
+//     try {
+//         const subscription = await stripe.subscriptions.create({
+//             customer: req.body.customerId,
+//             items: [{price: req.body.priceId}],
+//             // items: [{price: priceId}],
+//             expand: ['latest_invoice.payment_intent'],
+//         });
+//         if(!subscription) return res.status(404).send('No subscription found');
+//         res.send(subscription);
+//     } catch (error) {
+//         res.status(400).json({ message: 'Error creating subscription', error });
+//     }
+// };
+
 exports.createSubscription = async (req, res) => {
-    // priceId = "price_1PLUlvHiFNaBiQsH7Y22so1X";
+    // const transactionAmount = req.body.transactionAmount; // Assuming you're sending this in the request body
+    const transactionAmount = 2.79; // Assuming you're sending this in the request body
+    const  priceId  = "price_1PLUlvHiFNaBiQsH7Y22so1X";
+    const  prodId  = "prod_QBsWxC6kSHyoua"
+
+    console.log(priceId), console.log(prodId);
     console.log(req.body);
+
     try {
-        const subscription = await stripe.subscriptions.create({
-            customer: req.body.customerId,
-            items: [{price: req.body.priceId}],
-            // items: [{price: priceId}],
-            expand: ['latest_invoice.payment_intent'],
+        const totalAmountCharged = calculateTotalAmount(transactionAmount);
+
+        const session = await stripe.checkout.sessions.create({
+            // customer: req.body.client_reference_id,
+            billing_address_collection: 'auto',
+            mode: 'subscription',
+            payment_method_types: ['card'],
+            ui_mode: 'embedded',
+            // line_items: [
+            //     {
+            //         // plan: prodId,
+            //       price: priceId,
+            //       quantity: 1,
+          
+            //     },
+            //   ],
+            line_items: [{
+                // price_data: {
+                //   currency: 'usd',
+                //   product_data: {
+                //     name: 'Wallet Funds',
+                //     description: 'Credit Card Transaction Fees are added into your Total', // Add description here
+                //     images: ['https://www.trusselltrust.org/wp-content/uploads/sites/2/2020/11/tfc-wallet-icon@2x-300x300.png'], // Add image URL here
+
+                //   },
+                // //   unit_amount: totalAmountCharged * 100, // Convert to cents
+                // },
+                price: priceId,
+                quantity: 1,
+              }],
+    
+              metadata: {'userId': req.body.userId, 'client_reference_id': req.body.client_reference_id, 'amount': req.body.amount},
+              customer_email: req.body.email, // Prepopulated email
+              client_reference_id: req.body.client_reference_id, // This is for your internal reference
+            // mode: 'payment',
+            return_url: `${process.env.YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
         });
-        if(!subscription) return res.status(404).send('No subscription found');
-        res.send(subscription);
+        if(!session) return res.status(404).send('No session found');
+        res.send({clientSecret: session.client_secret});
     } catch (error) {
-        res.status(400).json({ message: 'Error creating subscription', error });
+        console.log(error);
+        res.status(400).json({ message: 'Error creating session', error });
     }
 };
