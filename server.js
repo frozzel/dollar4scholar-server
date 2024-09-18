6f8b754f6cb9fa762d7fb0b37cd21ba5036d8f9d
Dennis Hickox <frozzel@me.com>
2024-09-18 13:32:15 -0400

    server html temp

--- server.js
+++ server.js
@@ -46,6 +46,18 @@ app.use('/api/auth', authRouter);// use auth router
 const server = require('http').Server(app); // import http
 const PORT = process.env.PORT || 8080// define a port
 
+app.get('/', (req, res) => {
+  const date = new Date();
+  res.send(`<body style="background: #333; display: flex">
+      <div style="width: 30%; height: auto"></div>
+      <div style="display: flex-column; position: relative; top: 25%; width: 100%; height: 15%; box-shadow: 0 0 3px 2px #cec7c759; padding: 1em; border-radius: 8px;">
+      <h1 style="text-align: center; color: white;">🚀  Server Running  🚀</h1> \n 
+      <h3 style="text-align: center; color: white">${date.toString().slice(0, 24)}</h3>
+      </div><div style="width: 30%; height: auto"></div>
+      </body>`
+   );
+});
+
 server.listen(PORT,  () => {// start express server on port 8080
     console.log(`................................................`)
     console.log(`🚀  Server running on http://localhost:${PORT}, 🚀`)
